4a60925e2239e12082c09894029575f76ce7e59b
andresfelipedev <andresfelipedev@gmail.com>
2021-04-13 21:30:07 -0500

    Get opinionId from req.query in getComments

--- src/controllers/comments.controller.js
+++ src/controllers/comments.controller.js
@@ -3,12 +3,12 @@ const Comment = require('../models/Comment');
 const commentsController = {};
 
 commentsController.getComments = async (req, res) => {    
-    const { opinionId } = req.query;
+    const { opinionId } = req.query;    
     try {
         const comments = await Comment.find({ opinion: opinionId })
         .sort({ createdAt: -1 })
         .populate('author');
-        res.status(200).send(comments);
+        res.status(200).send(comments);        
     } catch (error) {
         res.status(500).send({
             message: 'Error while retrieving comments'            
@@ -17,14 +17,14 @@ commentsController.getComments = async (req, res) => {
 };
 
 commentsController.createComment = async (req, res) => {
-    const { body, opinionId } = req.body;
+    const { body, opinionId } = req.body;    
     const newComment = new Comment({ author: req.userId, opinion: opinionId, body });
     try {        
         const comment = await newComment.save();
         res.status(200).send({
             message: 'Comment created',
             commentId: comment._id
-        });
+        });        
     } catch (error) {
         res.status(500).send({
             message: 'Error while creating comment'            
