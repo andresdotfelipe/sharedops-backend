936236c3011088a89ee7fee61d8e426ee66974ac
andresfelipedev <andresfelipedev@gmail.com>
2021-04-18 15:02:00 -0500

    Find opinion and update it with new comment

--- src/controllers/comments.controller.js
+++ src/controllers/comments.controller.js
@@ -6,17 +6,27 @@ const commentsController = {};
 
 commentsController.createComment = async (req, res) => {
     const { body, opinionId } = req.body;
-    const author = await User.findById(req.userId).select(['+password', '+favoriteOpinions', '+createdAt', '+updatedAt']);
-    const newComment = new Comment({ author, body });
+    const newComment = new Comment({ author: req.userId, body });
     try {        
         const comment = await newComment.save();
-        const opinion = await Opinion.findById(opinionId).populate(['author', 'comments']);
-        opinion.comments.unshift(comment);
-        await opinion.save();
+        const updatedOpinion = await Opinion.findByIdAndUpdate(opinionId, 
+        { $push: { 'comments': { $each: [comment], $position: 0 } } }, { upsert: true, new: true })
+        .populate([
+            'author', 
+            { 
+                path: 'comments', 
+                populate: { 
+                    path: 'author', 
+                    select: 'name profilePicUrl', 
+                    model: 'User' 
+                } 
+            }
+        ])
+        .exec();                
         res.status(200).send({
             message: 'Comment created',
-            commentId: comment._id,
-            updatedOpinion: opinion
+            comment,            
+            updatedOpinion
         });        
     } catch (error) {
         res.status(500).send({
@@ -24,5 +34,27 @@ commentsController.createComment = async (req, res) => {
         });
     }
 };
+
+// commentsController.createComment = async (req, res) => {
+//     const { body, opinionId } = req.body;
+//     const author = await User.findById(req.userId).select(['name', 'profilePicUrl']);
+//     console.log(author);
+//     const newComment = new Comment({ author, body });
+//     try {        
+//         const comment = await newComment.save();
+//         const opinion = await Opinion.findById(opinionId).populate(['author', 'comments']);
+//         opinion.comments.unshift(comment);
+//         await opinion.save();
+//         res.status(200).send({
+//             message: 'Comment created',
+//             commentId: comment._id,
+//             updatedOpinion: opinion
+//         });        
+//     } catch (error) {
+//         res.status(500).send({
+//             message: 'Error while creating comment'            
+//         });
+//     }
+// };
 
 module.exports = commentsController;
