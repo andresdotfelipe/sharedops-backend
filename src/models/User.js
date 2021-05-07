da55ca79476b583fc5a59bf0f9487c9447810d3a
andresfelipedev <andresfelipedev@gmail.com>
2021-05-07 14:50:04 -0500

    Add description to UserSchema

--- src/models/User.js
+++ src/models/User.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcryptjs');
 
 const UserSchema = new Schema({
     name: { type: String, trim: true, required: true },
+    description: { type: String, trim: true },
     password: { type: String, select: false, required: true },
     profilePicUrl: { type: String, default: 'https://res.cloudinary.com/dxjc7e5te/image/upload/v1595711364/sharedops/profile-pics/default-user-pic_grjmh5.png' },
     favoriteOpinions: [{}]
