fe7663b46be433355a3ce42f6c29b7d76cdf4962
andresfelipedev <andresfelipedev@gmail.com>
2021-05-07 14:51:44 -0500

    Add getUserProfile and getUserOpinions routes

--- src/routes/api.js
+++ src/routes/api.js
@@ -5,20 +5,22 @@ const api = Router();
 const auth = require('../middlewares/auth');
 
 //  Controllers
-const { signIn, signUp, getUser, updateUserFavoriteOpinions } = require('../controllers/users.controller');
-const { getAllOpinions, getMyOpinions, getFavoriteOpinions, getOpinion, createOpinion } = require('../controllers/opinions.controller');
+const { signIn, signUp, getUser, getUserProfile, updateUserFavoriteOpinions } = require('../controllers/users.controller');
+const { getAllOpinions, getMyOpinions, getFavoriteOpinions, getUserOpinions, getOpinion, createOpinion } = require('../controllers/opinions.controller');
 const { createComment } = require('../controllers/comments.controller');
 
 //  Users routes
 api.post('/signin', signIn);
 api.post('/signup', signUp);
 api.get('/user', auth.checkToken, getUser);
+api.get('/user-profile', getUserProfile);
 api.put('/user/favorite-opinions', auth.checkToken, updateUserFavoriteOpinions);
 
 //  Opinions routes
 api.get('/opinions/all', getAllOpinions);
 api.get('/opinions/my-opinions', auth.checkToken, getMyOpinions);
 api.get('/opinions/favorites', auth.checkToken, getFavoriteOpinions);
+api.get('/opinions/user-opinions', getUserOpinions);
 api.get('/opinions/:id', getOpinion);
 api.post('/opinions', auth.checkToken, createOpinion);
 
