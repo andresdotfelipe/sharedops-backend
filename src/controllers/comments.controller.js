2757a4849d6f9bf56bbd0a3810694eb6d7d76a74
andresfelipedev <andresfelipedev@gmail.com>
2021-06-07 19:39:00 -0500

    Remove createComment controller comment

--- src/controllers/comments.controller.js
+++ src/controllers/comments.controller.js
@@ -1,5 +1,4 @@
 const Comment = require('../models/Comment');
-const User = require('../models/User');
 const Opinion = require('../models/Opinion');
 
 const commentsController = {};
@@ -34,27 +33,5 @@ commentsController.createComment = async (req, res) => {
         });
     }
 };
-
-// commentsController.createComment = async (req, res) => {
-//     const { body, opinionId } = req.body;
-//     const author = await User.findById(req.userId).select(['name', 'profilePicUrl']);
-//     console.log(author);
-//     const newComment = new Comment({ author, body });
-//     try {        
-//         const comment = await newComment.save();
-//         const opinion = await Opinion.findById(opinionId).populate(['author', 'comments']);
-//         opinion.comments.unshift(comment);
-//         await opinion.save();
-//         res.status(200).send({
-//             message: 'Comment created',
-//             commentId: comment._id,
-//             updatedOpinion: opinion
-//         });        
-//     } catch (error) {
-//         res.status(500).send({
-//             message: 'Error while creating comment'            
-//         });
-//     }
-// };
 
 module.exports = commentsController;
