78391d5036dbed936d5b35bc912a2b6d7d2233e6
andresdotfelipe <andresdotfelipecs@gmail.com>
2024-08-25 00:07:02 -0500

    Update CORS

--- src/server.js
+++ src/server.js
@@ -1,35 +1,36 @@
-const express = require('express');
-const cors = require('cors');
-const { serverConfig } = require('./config');
-const api = require('./routes/api');
+const express = require("express");
+const cors = require("cors");
+const { serverConfig } = require("./config");
+const api = require("./routes/api");
 
 const server = express();
 
 //  Settings
-server.set('port', serverConfig.port);
+server.set("port", serverConfig.port);
 
 //  Last element of whiteList is the front-end development origin.
 const whiteList = [
-    'https://sharedops.netlify.app',
-    'http://sharedops.netlify.app',
-    'http://localhost:3000'
+  "https://sharedops.netlify.app",
+  "http://sharedops.netlify.app",
+  "http://localhost:3000",
 ];
-server.use(cors({
+server.use(
+  cors({
+    origin: function (origin, callback) {
+      if (whiteList.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
-    origin: (origin, callback) => {
-        if (whiteList.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    }
-}));
+  })
+);
 server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
 
-
-//  Routes 
-server.get('/', (req, res) => res.send('Welcome to Sharedops API'));
-server.use('/api', api);
+//  Routes
+server.get("/", (req, res) => res.send("Welcome to Sharedops API"));
+server.use("/api", api);
 
 module.exports = server;
