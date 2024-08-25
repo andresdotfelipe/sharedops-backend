1044133f88d20bd7661f24689afcd0040cefed44
andresdotfelipe <andresdotfelipecs@gmail.com>
2024-08-25 11:58:40 -0500

    Update mongoose and database connection

--- src/database.js
+++ src/database.js
@@ -1,23 +1,14 @@
-const mongoose = require('mongoose');
-const { dbConfig } = require('./config');
+const mongoose = require("mongoose");
+const { dbConfig } = require("./config");
 
 const connectDB = () => {
-    const { mongodb_uri } = dbConfig;    
+  const { mongodb_uri } = dbConfig;
 
-    mongoose.connect(mongodb_uri, {
-        useNewUrlParser: true,
-        useFindAndModify: false,
-        useUnifiedTopology: true
-    })
-    .then(db => console.log('Connected to DB'))
-    .catch(error => {
-        console.log('DB error:', error);
-        setTimeout(connectDB(), 2000);
-    });
-
-    mongoose.connection.on('error', err => {
-        console.log('DB error:', err);
-        connectDB();
+  mongoose
+    .connect(mongodb_uri)
+    .then(() => console.log("Connected to DB"))
+    .catch((error) => {
+      console.log("DB connection error:", error);
     });
 };
 
